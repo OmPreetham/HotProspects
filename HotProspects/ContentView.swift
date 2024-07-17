9927d15fc8aad93a21741c6fc3e33748110017a5
Om Preetham <ompreethambandi@gmail.com>
2024-07-16 19:36:45 -0500

    finished day 80

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var output = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Text(output)
+            .task {
+                await fetchResults()
+            }
+    }
+    
+    func fetchResults() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
         }
-        .padding()
+        
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "\(error.localizedDescription)"
+            
+        }
     }
 }
 

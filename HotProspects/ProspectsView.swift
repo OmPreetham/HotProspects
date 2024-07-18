c8cad0ad86a66fbcbcfadd4f197f6da153b8e858
Om Preetham <ompreethambandi@gmail.com>
2024-07-18 14:41:21 -0500

    finished day 83

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Om Preetham Bandi on 7/17/24.
 //
 
+import CodeScanner
 import SwiftData
 import SwiftUI
 
@@ -28,27 +29,59 @@ struct ProspectsView: View {
     
     @Environment(\.modelContext) var modelContext
     @Query private var prospects: [Prospect]
+    
+    @State private var isShowingScanner = false
+    
+    @State private var selectedProspects = Set<Prospect>()
         
     var body: some View {
         NavigationStack {
-            List(prospects) { prospect in
+            List(prospects, selection: $selectedProspects) { prospect in
                 VStack(alignment: .leading) {
                     Text(prospect.name)
                         .font(.headline)
                     Text(prospect.email)
                         .foregroundStyle(.secondary)
                 }
+                .swipeActions {
+                    Button("Delete", systemImage: "trash", role: .destructive) {
+                        modelContext.delete(prospect)
+                    }
+                    
+                    if prospect.isContacted {
+                        Button("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue)
+                    } else {
+                        Button("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green)
+                    }
+                }
             }
             .navigationTitle(title)
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
-                    Button(action: {
-                        let prospect = Prospect(name: "Om Preetham", email: "ompreethambandi@gmail.com", isContacted: false)
-                        modelContext.insert(prospect)
-                    }) {
-                        Label("Scan", systemImage: "qrcode.viewfinder")
+                    Button("Scan QR", systemImage: "qrcode.viewfinder") {
+                        isShowingScanner = true
                     }
                 }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                
+                if selectedProspects.isEmpty == false {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Delete Selected", action: delete)
+                    }
+                }
+                
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
             }
         }
     }
@@ -61,6 +94,28 @@ struct ProspectsView: View {
         } else {
             let showContactedOnly = filter == .contacted
             _prospects = Query(filter: #Predicate { $0.isContacted == showContactedOnly }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+       isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            let person = Prospect(name: details[0], email: details[1], isContacted: false)
+
+            modelContext.insert(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
         }
     }
 }
